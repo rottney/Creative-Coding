67763f0334f92bbde5cbd4b8cdde3b55ddb391c9
Ryan Ottney <rottney123@gmail.com>
2024-08-14 14:21:52 -0400

    kind of drawing triangles

--- draw_shapes.py
+++ draw_shapes.py
@@ -1,3 +1,6 @@
+'''
+NOTE: "width" and "height" are not needed
+'''
 def draw_square(size):
 	def draw_horizontal_border(size):
 		for pixel in range(size):
@@ -18,11 +21,45 @@ def draw_square(size):
 	width = size * 2 - 1
 	height = size
 
-	for row in range(size):
+	print(f"Drawing square of size {size}:")
+	for row in range(height):
 		if row == 0 or row == size - 1:
 			draw_horizontal_border(size)
 		else:
 			draw_regular_row(size)
+	print()
+
+
+def draw_triangle(size):
+	def draw_top(size):
+		for _ in range(size - 1):
+			print(" ", end="")
+		print("*")
+		for _ in range(size - 1):
+			print(" ", end="")
+		print()
+
+	def draw_diagonal(size, row):
+		pass
+
+	def draw_bottom(size):
+		for pixel in range(size):
+			if pixel == size - 1:
+				print("*")
+			else:
+				print("* ", end="")
+	
+	width = size * 2 - 1
+	height = size
+
+	print(f"Drawing triangle of size {size}:")
+	for row in range(height):
+		if row == 0:
+			draw_top(size)
+		elif row == size - 1:
+			draw_bottom(size)
+		else:
+			draw_diagonal(size, row)
 	print()
 
 
@@ -31,8 +68,14 @@ def test_squares():
 		draw_square(i)
 
 
+def test_triangles():
+	for i in range(5):
+		draw_triangle(i)
+
+
 def main():
 	test_squares()
+	test_triangles()
 
 
 if __name__ == "__main__":
